6c972f3a354149e41c0d12e54a6fe0ecb9cfae04
Darsh <darsh2987@gmail.com>
2020-04-10 15:07:37 +0100

    front-page image-slider added - markup, styles and images.

--- front-page.php
+++ front-page.php
@@ -1,2 +1,17 @@
 <?php get_header(); ?>
+
+  <!-- Image slider -->
+  <section class="image-slider-container">
+    <div class="image-slider">
+      <div class="image-slide image-slider-hampton"></div>
+      <div class="image-slide image-slider-twickenham"></div>
+      <div class="image-slide image-slider-kingston"></div>
+      <div class="image-slide image-slider-teddington"></div>
+      <div class="image-slide image-slider-feltham"></div>
+      <div class="image-slide image-slider-hampton"></div>
+    </div>
+  </section>
+
+<!-- Grid to display All Locations -->
+  
 <?php get_footer(); ?>
